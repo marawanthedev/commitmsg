d640001fb4ad4668cbbc02671de74eba98e568d0
marwantigerlab <marwan.mostafa@tigerlab.com>
2023-11-05 20:24:57 +0800

    ukg: add button #I2G-12000

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. sss
-          testingsdsssss
+          testingsdsssss3
         </p>
         <a
           className="App-link"
